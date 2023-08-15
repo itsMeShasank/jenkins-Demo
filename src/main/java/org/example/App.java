a19e7f238c437b870164e9dbd0dc056e3dffd36a
shasank gadipilli <gadipillishasank@gmail.com>
2023-08-15 19:23:10 +0530

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addTwoNumbers(1,1));
+        System.out.println(calculator.subtractTwoNumbers(1,1));
+        System.out.println(calculator.multiplyTwoNumbers(1,1));
+    }
+}
---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+public class Calculator {
+
+    public int addTwoNumbers(int num1,int num2) {
+
+        return num1+num2;
+    }
+    public int subtractTwoNumbers(int num1,int num2) {
+
+        return num1-num2;
+    }
+
+    public int multiplyTwoNumbers(int num1,int num2) {
+        return num1 * num2;
+    }
+}
