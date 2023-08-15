88524fcc4b675d7082a3eeb3adf3216c718fd77f
shasank gadipilli <gadipillishasank@gmail.com>
2023-08-15 19:32:28 +0530

    second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.addTwoNumbers(1,1));
+        System.out.println(calculator.addTwoNumbers(1,1))
         System.out.println(calculator.subtractTwoNumbers(1,1));
         System.out.println(calculator.multiplyTwoNumbers(1,1));
     }
